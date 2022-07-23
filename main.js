4cd7ec81808c41f0cb71a8955bd3dff826aa89ed
jfranco3 <jfranco376@gmail.com>
2022-07-23 11:08:49 -0500

    edit main

--- main.js
+++ main.js
@@ -1,55 +1,40 @@
-'use strict';
-
 //pick a word for player to guess
-let word = 'citrus'
+let word = 'citrus';
+// console.log("ANSWER:", word)
 
-//create variable to represent letter spaces = empty array
-let answerArray = [];
-console.log(answerArray)
+//create variable to represent letter spaces using _'s in array 
+let board = ["_", "_", "_", "_", "_", "_",];
+console.log("GAME SPACES", board)
 
-for (let i = 0; i < word.length; i++) {
-    answerArray[i] = "_"
+//create turns allowed
+let guessesAllowed = 10
+
+//user inputs a guess
+let guessLetter = prompt("USER GUESS", 'Guess a Letter')
+// console.log ("YOU GUESSED:", guessLetter)
+
+//loop through the word to see if guess is correct
+while (guessesAllowed) {
+  let guessLetterIndex = word.indexOf(guessLetter)
+  // console.log("GUESSED LETTER", 'guessLetter')
+
+  if (guessLetterIndex > -1) {
+    board.splice(guessLetterIndex, 1, guessLetter)
+  }
+console.log("GAME SPACES", board)
+
+  //if guess is not in board
+  if (guessLetterIndex === -1) {
+    guessesAllowed--
+console.log("WRONG GUESS, TRY AGAIN", guessLetterIndex)
+  }
+
+  //if a win 
+  if (!board.includes("_")) {
+    return console.log("WIN!", 'You Guessed it! The word is ' +       word)
+  }
+console.log("GAME SPACES", board)
+
+  guessLetter = prompt("Guess A Letter")
 }
-
-//create arrays for wrong guesses
-let wrongGuess = [];
-
-//user inputs a guess--loop through the word to see if letter matches any letter in my word
-const guessedLetter = () => {
-for (let i = 0; i < word.length; i++) {
-    if (word[i] === guessedLetter){
-        guess[i].push
-        console.log(guessedLetter)
-}
-}
-}
-
-//if theres a match, input the letter at the corresponding index of variable-word 
-
-//if no match, alert 'try again'
-
-
-//create a counter for turns left
-
-//if out of turns, alert 'try again'
-// alert.
-
-//if a win, alert 'congrats'
-// alert.
-
-
-
-
-
-//fxn for looping through our word and the guessed letter
-// for (var i = 0; i < word.length; i++) {
-//     if (word[i] === guess) { //compare the letter guessed to value in each index of the word youre solving
-//       guesses[i].innerHTML = guess; //add the guess to the baord at the same index as the word ...if true splice(add) letter into whatever index of guesses array
-//     } 
-//   }
-
-
-// const getPrompt = () => {
-//     console.log( guess(word));
-//     getPrompt();
-// };
+// console.log("NO MORE TURNS", 'You ran out of turns. Try Again!')
